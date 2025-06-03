7ca5219e0971b24c308ba105c38dd052b6823857
Mauricio Tovar <mauricio.tovar.gil@gmail.com>
2025-06-03 13:21:58 -0500

    test

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # uiEasyPyment
+flutter test --coverage
+coverage/lcov.info
+
+genhtml coverage/lcov.info -o coverage/html
+coverage/html/index.html
+start coverage/html/index.html
+
+
+dart pub global activate lcov_report
+dart pub global run lcov_report:report
+
+
+// alternativa 3
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,34 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:first_app/src/features/auth/presentation/app.dart';
-import 'package:flutter/material.dart';
+import 'package:first_app/src/features/auth/application/login_service.dart';
+import 'package:first_app/src/features/auth/domain/usecases/LoginUseCase.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
+class MockLoginUseCase extends Mock implements LoginUseCase {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const Auth());
+  late MockLoginUseCase mockLoginUseCase;
+  late LoginService loginService;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  setUp(() {
+    mockLoginUseCase = MockLoginUseCase();
+    loginService = LoginService(mockLoginUseCase);
   });
-}
+
+    test('login returns true when credentials are correct', () async {
+      // Arrange
+      String email = 'test@email.com';
+      String password = '1234';
+      when(mockLoginUseCase.execute(email, password))
+          .thenAnswer((_) async => true);
+
+      // Act
+      final result = await loginService.login(email, password);
+  
+      // Assert
+      expect(result, true);
+      verify(mockLoginUseCase.execute('test@email.com', '1234')).called(1);
+    });
+  }
